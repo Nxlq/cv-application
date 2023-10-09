99a7906b1c7118cea89137d963632d5e2fba4e42
JeffW <116536323+Nxlq@users.noreply.github.com>
2023-10-09 17:15:46 -0400

    add: education dummy content and inital style - hard coded, not interactive

--- src/components/ResumePreview.jsx
+++ src/components/ResumePreview.jsx
@@ -4,14 +4,37 @@ function ResumeHead({ personalDetails }) {
   const { fullName, email, phoneNumber, location } = personalDetails;
 
   return (
-    <div className="resume-head__wrapper">
+    <section className="resume-head__wrapper">
       <h2>{fullName ? fullName : "Your Full Name"}</h2>
       <p>
         {email ? email : "emailhere@example.com"} |{" "}
         {phoneNumber ? phoneNumber : "123-456-7890"} |{" "}
         {location ? location : "City, State"}
       </p>
-    </div>
+    </section>
+  );
+}
+
+function EducationSection() {
+  return (
+    <section>
+      <h3>Education</h3>
+      <div className="education__wrapper">
+        <div>
+          <h4>Harvard Univeristy (HU)</h4>
+          <h5 className="degree-info">
+            Bachelor of Astrochemistry Quantam Computing Cell Engineering
+          </h5>
+          <span className="gpa-info">
+            Major GPA: 5.2/4.0; Overall GPA: 5.0/4.0
+          </span>
+        </div>
+        <div className="date-location">
+          <span className="location">Brooklyn, Ny</span>
+          <span className="date">May 2021</span>
+        </div>
+      </div>
+    </section>
   );
 }
 
@@ -19,6 +42,7 @@ export default function ResumePreview({ personalDetails }) {
   return (
     <div className="resume-preview__container">
       <ResumeHead personalDetails={personalDetails} />
+      <EducationSection />
     </div>
   );
 }
